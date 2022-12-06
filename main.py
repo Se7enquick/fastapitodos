3fc04de188ca2688740e6a8fb543a1d37c26b326
Vladyslav Koval <vladyslav.koval@raccoongang.com>
2022-12-06 02:11:54 +0200

    To-do API using fastAPI

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 import models
 from database import engine
 from routers import auth, todos, users, address
-from company import companyapis, dependencies
 
 app = FastAPI()
 
@@ -12,10 +11,6 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
 app.include_router(address.router)
-app.include_router(companyapis.router,
-                   prefix='/companyapis', tags=['companyapis'],
-                   responses={418: {'description': 'Internal use only'}},
-                   dependencies=[Depends(dependencies.get_token_header)])
 
 
 
